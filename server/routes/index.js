e38919dbaceff73928494db89a9ced7286f25c36
Юра <yura@MacBook-Pro-Ana.local>
2019-03-04 08:38:15 +0200

    Added translation for RU and LV

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,15 @@ router.get('/', (req, res) => res.render('home'))
 
 router.get('/orders', (req, res) => res.render('orders/main'))
 
-router.get('/orders/submit', (req, res) => res.render('orders/submit-order'))
+router.get('/orders/add', (req, res) => res.render('orders/submit-order'))
+
+router.post('/orders/submit', (req, res) => {
+  const {city, price, number} = req.body
+  res.send({order: {
+    city,
+    price,
+    number
+  }})
+
+})
 module.exports = router
