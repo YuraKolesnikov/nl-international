302af64a2bc4cb08916e08245a61356e312b2ec8
Jurijs Kolesnikovs <jurijs_kolesnikovs@inbox.lv>
2019-03-12 17:27:00 +0200

    Commented out connecting interface

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost/MongoTut', { useNewUrlParser: true })
+/* mongoose.connect('mongodb://localhost/MongoTut', { useNewUrlParser: true })
 .then(() => console.log('Connected to MongoDB'))
-.catch(e => console.log('Error!', e))
+.catch(e => console.log('Error!', e)) */
 
 module.exports = { mongoose }
