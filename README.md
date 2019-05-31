3f40384879d1cf5abc459bd991b486e89f1bfa28
Godfather <jurijs_kolesnikovs@inbox.lv>
2019-06-01 01:48:17 +0300

    Added client boilerplate and separated server to workspace

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 This branch purpose is replacing `.hbs` 'Front End' with real Vue.js client
 
 ### Todo List:  
-- [ ] Implement workspaces
-- [ ] Initiate Vue.js client
+- [x] Implement workspaces
+- [x] Initiate Vue.js client
 - [ ] Add vue-bootstrap
 - [ ] Add axios for backend requests
 - [ ] Implement Vuex
---
+++ server/server.js
@@ -0,0 +1,86 @@
+/* Dependencies */
+const dotenv = require('dotenv').config()
+const express = require('express')
+const hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const i18n = require('i18n-express')
+const path = require('path')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
+const passport = require('passport')
+const morgan = require('morgan')
+const cors = require('cors')
+
+/* Modules */
+const config = require('./config/config')
+const { apiRouter } = require('./api/routes/api.router')
+
+const passportStrategy = require('./auth/passport')
+passportStrategy(passport)
+
+/* Setting up application */
+const app = express()
+
+/* Middleware */
+const publicPath = 'public'
+
+app.use(express.static(publicPath))
+app.use(morgan('dev'))
+app.use(cors())
+
+app.engine('.hbs', hbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: {
+    inc: (i) => parseInt(i) + 1
+  }
+}))
+
+app.set('view engine', '.hbs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.use(cookieParser())
+app.use(i18n({
+  translationsPath: path.join(__dirname, 'i18n'), // <--- use here. Specify translations files path.
+  siteLangs: ['ru', 'lv'],
+  defaultLang: 'ru',
+  textsVarName: 'local',
+  cookieLangName: 'ulang'
+}));
+app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'Godfather123#@!',
+  resave: true,
+  saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.error = req.flash('error')
+  res.locals.user = req.user || null
+  next()
+})
+
+/* Router */
+app.get('/', (req, res) => res.render('home'))
+app.get('/api', (req, res, next) => {
+  res.json({msg: 'API'})
+})
+app.use('/', apiRouter)
+/* app.use('/orders', indexRouter)
+app.use('/user', userRouter)
+app.use('/admin', adminRouter) */
+
+/* Port setup */
+app.listen(config.port, () => console.log(`App started on ${config.port}`))
+
+const http = require("http")
+setInterval(function () {
+  http.get("http://nl-delivery.herokuapp.com")
+}, 300000) // every 5 minutes
