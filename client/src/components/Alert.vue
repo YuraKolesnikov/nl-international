7b3dfac01cd7350961b8446a22cdaf255ea90164
YuraKolesnikov <jurijs_kolesnikovs@inbox.lv>
2019-06-05 16:23:00 +0300

    Added abstract alert

--- client/src/components/Alert.vue
+++ client/src/components/Alert.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    
+  <div class="alert my-3" :class="`alert-${data.type}`" style="max-width: 540px; width: 100%; margin: auto;">
+    {{data.message}}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
   </div>
 </template>
+<script>
+export default {
+  props: {
+    data: Object
+  }
+}
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,6 +3,11 @@
     <h1>This is a home page</h1>
     <div class="container">
       <div class="row">
+        <div class="col-sm-12" v-for="item in alertData" :key="item.type">
+          <Alert :data="item"/>
+        </div>
+      </div>
+      <div class="row">
         <div v-for="order in orders" :key="order._id" class="order card col-md-3">
           <p><b>{{order}}</b></p>
           <router-link :to="`/orders/edit/${order._id}`">Edit order</router-link>
@@ -13,10 +18,18 @@
 </template>
 <script>
 import OrderService from '@/services/OrderService'
+import Alert from '@/components/Alert.vue'
 export default {
+  components: {
+    Alert
+  },
   data() {
     return {
-      orders: []
+      orders: [],
+      alertData: [
+        { type: 'success', message: 'User registered!' },
+        { type: 'danger', message: 'Login is taken!' }
+      ]
     }
   },
   mounted() {
