a6c42e453eaa035538f0968141839517a602edcd
YuraKolesnikov <jurijs_kolesnikovs@inbox.lv>
2019-06-05 16:15:51 +0300

    Added bootstrap

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="about">
     <h1>This is a home page</h1>
-    <div v-for="order in orders" :key="order._id" class="order">
-      <p><b>{{order}}</b></p>
-      <router-link :to="`/orders/edit/${order._id}`">Edit order</router-link>
+    <div class="container">
+      <div class="row">
+        <div v-for="order in orders" :key="order._id" class="order card col-md-3">
+          <p><b>{{order}}</b></p>
+          <router-link :to="`/orders/edit/${order._id}`">Edit order</router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
